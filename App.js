c7e0e53813a7202f1b518214bcdf12693ae008a3
MiguelSoares23 <169060996+MateusJuan@users.noreply.github.com>
2025-12-15 10:51:16 -0300

    aaaaaaaaaaaaaaa

--- App.js
+++ App.js
@@ -1,6 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
@@ -12,19 +10,10 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Inicio'>
-        <Stack.Screen name='Inicio' component={TelaInicio} options={{headerShown: false}}/>
-        <Stack.Screen name='Noticia' component={TelaNoticia} options={{headerShown: false}}/>
+      <Stack.Navigator initialRouteName="Inicio">
+        <Stack.Screen name="Inicio" component={TelaInicio} options={{ headerShown: false }} />
+        <Stack.Screen name="Noticia" component={TelaNoticia} options={{ headerShown: false }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- screens/TelaInicio.js
+++ screens/TelaInicio.js
@@ -1,43 +1,49 @@
-import React, { useState } from "react";
-import { FlatList, StyleSheet, View } from 'react-native';
-import NoticiaCard from '../components/NoticiaCard';
-import api from '../services/api';
+import React, { useState, useEffect } from "react";
+import { View, FlatList, StyleSheet } from "react-native";
+import NoticiaCard from "../components/NoticiaCard";
+import api from "../services/api";
 
 const TelaInicio = ({ navigation }) => {
-    const [noticias, setNoticias] = useState([]);
+  const [noticias, setNoticias] = useState([]);
 
-    const fetchNoticias = async () => {
-        try {
-            const response = await api.post("article/getArticles", {
-            lang: "por",
-            resultType: "articles",
-            articlesCount: 20,
-        });
-            setNoticias(response.data.articles.results);
-        } catch (error) {
-            console.error('Erro ao consultar noticias:', error);
-        }
-    };
+  const fetchNoticias = async () => {
+    try {
+      const response = await api.post("article/getArticles", {
+        lang: "por",
+        resultType: "articles",
+        articlesCount: 20,
+      });
+      setNoticias(response.data.articles.results);
+    } catch (error) {
+      console.error("Erro ao buscar notícias:", error);
+    }
+  };
 
-    useEffect(() => {
-        const unsubscribe = navigation.addListener('focus', fetchNoticias);
-        return unsubscribe;
-    }, [navigation]);
+  useEffect(() => {
+    fetchNoticias();
+  }, []);
 
-    const handlePress = (noticia) => {
-        navigation.navigate('Noticia', { noticia })
-    };
-
-    return (
-        <View style={styles.container}>
-            <FlatList style={{ flex: 1, width: '100%' }} contentContainerStyle={{ paddingHorizontal: 16, paddingBottom: 24 }} data={noticias} keyExtractor={(item) => item.uri} renderItem={({ item }) => (
-                //<Text style={{ fontSize: 18, padding: 10 }}>{item.nome}</Text>
-                <NoticiaCard noticia={item} onPress={() => handlePress(item)} />
-            )}
-            />
-        </View>
-    );
-
-}
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={noticias}
+        keyExtractor={(item) => item.uri}
+        renderItem={({ item }) => (
+          <NoticiaCard
+            noticia={item}
+            onPress={() => navigation.navigate("Noticia", { noticia: item })}
+          />
+        )}
+      />
+    </View>
+  );
+};
 
 export default TelaInicio;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+});
